92a51f6ebbc44dd4a6d518df4200800d5d0effa3
Gabriel Henrique <biel161760@gmail.com>
2021-10-02 14:08:42 -0300

    atualiza usandoVar1 e adiciona usandoVar2

--- Fundamentos/usandoVar1.js
+++ Fundamentos/usandoVar1.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+{
+    {
+        {
+            { var sera = 'Será???' }
+        }
+    }
+}
+console.log(sera);
+
+function teste() {
+    var local = 123
+}
+teste()
+console.log(local);
+//Erro! A variável 'algo' não será exibida porque só foi definida dentro da função
+//evite criar variáveis que vão para o escopo global
