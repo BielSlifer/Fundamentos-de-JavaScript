a680e560f46ccda8cf5963c46f79db40729ed907
Gabriel Henrique <biel161760@gmail.com>
2021-10-01 22:23:32 -0300

    atualiza function1.js

--- Fundamentos/function1.js
+++ Fundamentos/function1.js
@@ -1,2 +1,16 @@
 //função sem retorno
-function ImprimirSoma(a, b)
+function ImprimirSoma(a, b) {
+    console.log(a + b);
+}
+
+ImprimirSoma(2) // resultará em NaN
+ImprimirSoma(2, 4, 7, 1, 10, 40); 
+/*O programa fará a soma dos dois primeiros números e irá ignorar o restante*/
+
+
+//função com retorno
+function Soma(a, b = 0) {
+    return a + b
+}
+console.log(Soma(2, 3));
+console.log(Soma(2));
