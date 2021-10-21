3f9d919b528c40e5a6a47a78d0d335d4b71e3c6a
Gabriel Henrique <biel161760@gmail.com>
2021-10-21 15:43:51 -0300

    atualiza usandoLet1.js

---
+++ Fundamentos/usandoLet1.js
@@ -0,0 +1,9 @@
+var numero = 1
+{
+    let numero = 2
+    console.log('Dentro =', numero);
+}
+console.log('Fora =', numero);
+/* Variáveis declaradas com Var tem escopo global e escopo de funcão. 
+   Variáveis declaradas com Let tem escopo global, escopo de função e escopo de bloco. 
+*/
