f9eb95ae0940a6f631524f01ede4cb516b091664
Gabriel Henrique <biel161760@gmail.com>
2021-10-22 17:12:20 -0300

    atualiza object2.js

--- Fundamentos/object2.js
+++ Fundamentos/object2.js
@@ -1,2 +1,10 @@
 console.log(typeof Object); //retorna function.
 console.log(typeof new Object); //cria um novo objeto e retorna object.
+
+const Cliente = function() {}
+console.log(typeof Cliente);
+console.log(typeof new Cliente);
+
+class Produto {} // ES 2015 (ES6)
+console.log(typeof Produto);
+console.log(typeof new Produto());
