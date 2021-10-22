49f18a9ce59f9b7658cff2f06537691d8b3cfefd
Gabriel Henrique <biel161760@gmail.com>
2021-10-22 16:58:26 -0300

    atualiza hoisting.js

--- Fundamentos/hoisting.js
+++ Fundamentos/hoisting.js
@@ -1,4 +1,18 @@
 console.log('a =', a);
 var a = 2
 console.log('a =', a);
-//em JavaScript a variável é 'içada para cima' sem gerar erro caso tenha sido definida abaixo.
+/*Em JavaScript a variável é 'içada para cima' sem gerar erro caso tenha sido definida abaixo. 
+Não tem uso práico, apenas é possível em JS.*/
+
+function teste() {
+    console.log('a =', a);
+    var a = 2
+    console.log('a =', a);
+}
+
+teste(); //na função ela sofre o içamento da mesma forma
+
+console.log('b =', b);
+let b = 2
+console.log('b =', b);
+//Variável Let gera erro
