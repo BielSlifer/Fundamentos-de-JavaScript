bd4518072d57fd92d733fbc5b12116555d05bd14
Gabriel Henrique <biel161760@gmail.com>
2021-10-21 21:43:47 -0300

    atualiza usandoLetEmLoop1.js

--- Fundamentos/usandoLetEmLoop1.js
+++ Fundamentos/usandoLetEmLoop1.js
@@ -1,5 +1,6 @@
-for (var i = 1; i < 20; i++) {
+for (let i = 1; i < 20; i++) {
     console.log(i);
 }
 
 console.log('i =', i);
+//gera um erro pela variável ter sido definida com Let dentro do escopo do bloco
