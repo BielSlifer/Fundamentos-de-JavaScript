9e451f744d5a7cc839fbdd03bed12d165583a618
Gabriel Henrique <biel161760@gmail.com>
2021-10-25 17:05:10 -0300

    atualiza atribuicao.js

---
+++ Fundamentos/atribuicao.js
@@ -0,0 +1,20 @@
+const a = 7
+let b = 3
+
+b += a // faz uma atribuição ao valor de b com o valor de a | b = b + a
+console.log(b);
+
+b -= 4 // faz uma subtração ao valor de b com o valor de 4 | b = b - 4
+console.log(b);
+
+b *= 2 // faz uma multiplicação ao valor de b com o valor de 2 | b = b * 2
+console.log(b);
+
+b /= 2 // faz uma divisão ao valor de b com o valor de 2 | b = b / 2
+console.log(b);
+
+b %= 2 // faz uma divisão ao valor de b com o valor de 2 | b = b % 2
+console.log(b);
+
+b **= 2 // faz uma potenciação ao valor de b com o valor de 2 | b = b ** 2
+console.log(b);
